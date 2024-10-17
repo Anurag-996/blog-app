afb410d3ebb5f019cc86d24e9076f68ee3691da6
Anurag-996 <112651151+Anurag-996@users.noreply.github.com>
2024-10-17 13:54:14 +0530

    Update RedisConfig.java

--- src/main/java/com/blog/BloggingApp/Config/RedisConfig.java
+++ src/main/java/com/blog/BloggingApp/Config/RedisConfig.java
@@ -1,14 +1,38 @@
 package com.blog.BloggingApp.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${SPRING_REDIS_HOST}")
+    private String redisHost;
+
+    @Value("${SPRING_REDIS_PORT}")
+    private int redisPort;
+
+    @Value("${REDIS_PASSWORD:}") // Default to empty string if not set
+    private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        
+        // Set the password if it's not empty
+        if (!redisPassword.isEmpty()) {
+            configuration.setPassword(redisPassword);
+        }
+
+        return new LettuceConnectionFactory(configuration);
+    }
 
     @Bean(name = "booleanRedisTemplate")
     public RedisTemplate<String, Boolean> booleanRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
